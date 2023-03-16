ed26267e6beed26448600c89ad30df13145915dc
Antoine <98873193+Antoine-Mtx@users.noreply.github.com>
2023-03-16 14:39:25 +0100

    add error and loading to Registration

--- src/containers/authentication/RegistrationContainer.js
+++ src/containers/authentication/RegistrationContainer.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import RegistrationForm from '../../components/authentication/RegistrationForm.js';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { register } from '../../actions/authenticationActions.js';
 
 const RegistrationContainer = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const loading = useSelector(state => state.authentication.loading);
+  const error = useSelector(state => state.authentication.error); 
 
-  const handleSubmit = (formData) => {
+  const handleRegistration = (formData) => {
     dispatch(register(formData))
       .then(() => navigate('/login'))
       .catch((error) => console.log(error));
   };
-  
-  
 
-  return <RegistrationForm onSubmit={handleSubmit} />;
-};
+  return (
+    <>
+      <RegistrationForm onSubmit={handleRegistration} loading={loading} error={error} />
+    </>
+  };
 
 export default RegistrationContainer;
